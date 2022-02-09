902a9ca7c026fd36d2b1a1806f863367a6150a3b
felipemichelin <contato@felipemichelin.com>
2022-02-09 04:14:46 -0300

    a

--- gulpfile.js
+++ gulpfile.js
@@ -23,51 +23,6 @@ gulp.task('serve', serve({
 
 
 
-// cleanup the build output
-gulp.task('clean-build', function () {
-  return gulp.src(buildDest, {read: false})
-    .pipe(clean());
-});
-// Delete our old css files
-gulp.task('clean-css', function () {
-  return gulp.src(buildDest + "/css/**/*", {read: false})
-    .pipe(clean());
-});
-// Delete our old js files
-gulp.task('clean-js', function () {
-  return gulp.src(buildDest + "/js/**/*", {read: false})
-    .pipe(clean());
-});
-
-
-
-// Compile the templates into html
-// We don't need a template tool for this, just copy the
-// html files to the build folder
-gulp.task("render", function () {
-  gulp.src([buildSrc + '/pages/**/[!_]*.html'])
-    .pipe(gulp.dest(buildDest))
-});
-
-
-
-// Compile SCSS files to CSS
-gulp.task("scss", ['clean-css'], function () {
-  gulp.src(buildSrc + "/scss/main.scss")
-    .pipe(sass({
-      outputStyle: "compressed"
-    })
-    .on('error', sass.logError))
-    .pipe(gulp.dest(buildDest + "/css"))
-});
-
-
-
-// simplest possible noddy js management
-gulp.task("js", function () {
-  gulp.src(buildSrc + "/js/**/*.js")
-    .pipe(gulp.dest(buildDest + '/js'))
-});
 
 
 
@@ -114,13 +69,6 @@ gulp.task("get:routes", function () {
 
 
 
-// Watch working folders for changes
-gulp.task("watch", ["build"], function () {
-  gulp.watch(buildSrc + "/scss/**/*", ["scss"]);
-  gulp.watch(buildSrc + "/js/**/*", ["js"]);
-  gulp.watch(buildSrc + "/pages/**/*", ["render"]);
-  gulp.watch(buildSrc + "/entries.json", ["render"]);
-});
 
 
 
